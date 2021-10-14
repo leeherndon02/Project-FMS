b55ad903f3e0e33a1aee9a2a7679c1c79419850e
leeherndon02 <69217419+leeherndon02@users.noreply.github.com>
2021-10-14 13:49:40 -0700

    I made a box with text in it

--- penny-pinching/sketch.js
+++ penny-pinching/sketch.js
@@ -1,9 +1,17 @@
 function setup() 
   {
     createCanvas(1366, 768);
+    background(220);
+    textSize(40);
   }
   
   function draw() 
   {
-    background(220);
+    
+    text('Penny Pinching', 10, 50);
+    noFill();
+    rect(9, 15, 280, 50);
+    
+    
+
   }
