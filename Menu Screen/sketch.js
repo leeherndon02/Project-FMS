1f2dc950c00322514fa9d7227a3ba628816137cd
Roberto De La Rosa <rjdelarosaua@gmail.com>
2021-10-14 13:22:05 -0700

    commit heyo

---
+++ Menu Screen/sketch.js
@@ -0,0 +1,48 @@
+var mode; //determines if game has started
+var game1 = 'Penny Pinching';
+//var game2 = 'Tracing Letters';
+//var game3 = 'Typing';
+
+var winH = 731/2;
+var winW = 1536/2;
+
+function setup() {
+    createCanvas(1536,731); //height 731, width 1536
+    background(200);
+    mode = 0; //initially, game not started
+    textSize(20);
+
+  }
+  
+  function draw() {
+    clear();
+
+    if (mode == 0){
+      text(game1, winW, winH);
+    }
+
+    if (mode == 1){
+      ellipse(100, 100, 100, 100);
+    }
+
+  }
+
+  function mouseClicked(){
+    if (isMouseInsideText(game1, winW, winH)) {
+      mode=1;
+    }
+      
+  }
+
+  function isMouseInsideText(game1, winW, winH) {
+    const messageWidth = textWidth(game1);
+    const messageTop = winH - textAscent();
+    const messageBottom = winH + textDescent();
+  
+    return mouseX > winW && mouseX < winW + messageWidth &&
+      mouseY > messageTop && mouseY < messageBottom;
+  }
+ 
+  
+
+  
