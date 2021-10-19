ed58fb5886107be41ecb37ba29463791c17f44b9
Roberto De La Rosa <rjdelarosaua@gmail.com>
2021-10-19 10:55:06 -0700

    first draft of menu screen

--- Menu Screen/sketch.js
+++ Menu Screen/sketch.js
@@ -1,53 +1,58 @@
-var mode; //determines if game has started
-var game1 = 'HELP';
-//var game2 = 'Tracing Letters';
-//var game3 = 'Typing';
 
-var winH = 731/2;
-var winW = 1536/2;
+  var game1 = 'Penny Pinching';
+  var game2 = 'Tracing Letters';
+  var game3 = 'Typing';
+  var title = 'Train Your FMS'
+  let img;
 
-function setup() {
-    createCanvas(windowWidth, windowHeight); //height 731, width 1536
-    background(500, 200, 100);
-    mode = 0; //initially, game not started
-    textSize(20);
-    
-
+  function preload(){
+    img = loadImage('keyboard.png');
   }
-  
-  function draw() {
-    //clear();
-    
-    if (mode == 0){
-      text(game1, winW, winH, 20, 20);
-
-      noFill();
-      rect(winW, winH, 60, 20);
+  function setup() {
+      createCanvas(windowWidth, windowHeight); //height 731, width 1536
+      frameRate(10);
       
     }
-
-    if (mode == 1){
-      ellipse(100, 100, 100, 100);
-    }
-
-  }
-
-  function mouseClicked(){
-    if (isMouseInsideText(game1, winW, winH)) {
-      mode=1;
-    }
+    
+    function draw() {
+      background(160, 160, 160);
       
-  }
+      fill(204, 102, 0);
+      circle(280, 350, 220); //penny
 
-  function isMouseInsideText(game1, winW, winH) {
-    const messageWidth = textWidth(game1);
-    const messageTop = winH - textAscent();
-    const messageBottom = winH + textDescent();
-  
-    return mouseX > winW && mouseX < winW + messageWidth &&
-      mouseY > messageTop && mouseY < messageBottom;
-  }
- 
-  
+      
+      textSize(300);
+      fill(0, 102, 153);
+      textAlign(CENTER);
+      text('A', 768, 435);//tracing game letter
+      //768, 325
 
+      image(img, 1035, 300, 350, 100); //keyboard
+
+      fill(0,0,0);
+      textSize(40);
+      textAlign(CENTER);
+      text(title,windowWidth*.50, windowHeight*.15);
+      
+      textSize(30);
+      stroke(50);
+      textAlign(RIGHT);
+      text(game1, windowWidth*.25, windowHeight/2);
+      textAlign(CENTER);
+      text(game2, windowWidth*.50, windowHeight/2);
+      
+      textAlign(LEFT);
+      text(game3, windowWidth*.75, windowHeight/2);
+      
+      //text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
+    }
+
+    function windowResized() {
+      resizeCanvas(windowWidth, windowHeight);
+
+      
+    }
   
+    
+
+    
