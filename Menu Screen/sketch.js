cf7f7913a9b10e7e7b0595748722eef8d52dd11e
Roberto De La Rosa <rjdelarosaua@gmail.com>
2021-10-19 12:56:09 -0700

    Updated Menu alignment

--- Menu Screen/sketch.js
+++ Menu Screen/sketch.js
@@ -6,28 +6,31 @@
   let img;
 
   function preload(){
-    img = loadImage('keyboard.png');
+    img = loadImage('keyboard.png'); 
   }
+
   function setup() {
       createCanvas(windowWidth, windowHeight); //height 731, width 1536
-      frameRate(10);
+      //frameRate(10);
       
     }
-    
+  
     function draw() {
       background(160, 160, 160);
       
       fill(204, 102, 0);
-      circle(280, 350, 220); //penny
+      circle(windowWidth*.18, windowHeight/2, 220);
+      //circle(280, 350, 220); //penny
 
-      
       textSize(300);
       fill(0, 102, 153);
-      textAlign(CENTER);
-      text('A', 768, 435);//tracing game letter
+      textAlign(CENTER, CENTER);
+      text('A', windowWidth*.50, windowHeight*.50);//tracing game letter
       //768, 325
-
-      image(img, 1035, 300, 350, 100); //keyboard
+      
+      //(img, )
+      
+      image(img, windowWidth*.68, windowHeight*.40, windowWidth*.20, windowHeight*.20); //keyboard
 
       fill(0,0,0);
       textSize(40);
@@ -36,15 +39,16 @@
       
       textSize(30);
       stroke(50);
-      textAlign(RIGHT);
-      text(game1, windowWidth*.25, windowHeight/2);
-      textAlign(CENTER);
+      textAlign(CENTER); //penny game title
+      text(game1, windowWidth*.18, windowHeight/2);
+
+      textAlign(CENTER);//tracing title
       text(game2, windowWidth*.50, windowHeight/2);
       
-      textAlign(LEFT);
+      textAlign(LEFT);//typing title
       text(game3, windowWidth*.75, windowHeight/2);
       
-      text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
+      //text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
     }
 
     function windowResized() {
