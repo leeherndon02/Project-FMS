cae513f5ec4e5829d755b31e26436153bf15f73e
Roberto De La Rosa <rjdelarosaua@gmail.com>
2021-10-19 19:34:42 -0700

    New Tracing File

--- Menu Screen/letter_tracing.js
+++ Menu Screen/letter_tracing.js
@@ -1,22 +1,19 @@
-function setup() 
-  {
+class TracingExercise{
+  constructor (){
+
+  }
+
+  setCanvas() {
     createCanvas(windowWidth, windowHeight);
     button = createButton('MENU');
     background(160, 160, 160);
-    //frameRate(10);
     
   }
-  
-  function draw() 
-  {
-  
-    
 
+  setElements() {
     button.position(windowWidth*.05, windowHeight*.75);
     button.size(100, 100);
-
-    
-
+  
     textSize(800);
     fill(0, 102, 153);
     textAlign(CENTER);//traced letter
@@ -26,7 +23,7 @@ function setup()
     fill(0,204,0,63);
     strokeWeight(2);
     circle(550, 600, 100);
-
+  
     fill(0,0,0);
     rect(1160, 460, 300, 200);//points box
     fill(255, 255, 255);
@@ -34,7 +31,7 @@ function setup()
     textAlign(CENTER);
     text('Points:', 1300, 520);
     text('500', 1300, 600);
-
+  
     
     noStroke();
     fill(0,0,0);
@@ -42,7 +39,7 @@ function setup()
     textSize(40);
     textAlign(CENTER);//title
     text('Tracing Letters!',windowWidth*.12, windowHeight*.15);
-
+  
     
     stroke(255, 255, 255);
     strokeWeight(10);
@@ -53,12 +50,5 @@ function setup()
     strokeWeight(1);
     triangle(619, 135, 555, 200, 610, 225);
     noStroke();
-
-    //text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
-  }
-
-  function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-
-    
-  }
+  } 
+}
--- Menu Screen/sketch.js
+++ Menu Screen/sketch.js
@@ -1,9 +1,4 @@
-
-  var game1 = 'Penny Pinching';
-  var game2 = 'Tracing Letters';
-  var game3 = 'Typing';
-  var title = 'Train Your FMS'
-  let img;
+  let img, letter1;
 
   function preload(){
     img = loadImage('keyboard.png'); 
@@ -11,52 +6,56 @@
 
   function setup() {
       createCanvas(windowWidth, windowHeight); //height 731, width 1536
-      //frameRate(10);
-      
-    }
-  
-    function draw() {
       background(160, 160, 160);
-      
-      fill(204, 102, 0);
-      circle(windowWidth*.18, windowHeight/2, 220);
-      //circle(280, 350, 220); //penny
-
-      textSize(300);
-      fill(0, 102, 153);
-      textAlign(CENTER, CENTER);
-      text('A', windowWidth*.50, windowHeight*.50);//tracing game letter
-      //768, 325
-      
-      //(img, )
-      
-      image(img, windowWidth*.68, windowHeight*.40, windowWidth*.20, windowHeight*.20); //keyboard
-
-      fill(0,0,0);
-      textSize(40);
-      textAlign(CENTER);
-      text(title,windowWidth*.50, windowHeight*.15);
-      
-      textSize(30);
-      stroke(50);
-      textAlign(CENTER); //penny game title
-      text(game1, windowWidth*.18, windowHeight/2);
-
-      textAlign(CENTER);//tracing title
-      text(game2, windowWidth*.50, windowHeight/2);
-      
-      textAlign(LEFT);//typing title
-      text(game3, windowWidth*.75, windowHeight/2);
-      
-      //text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
-    }
-
-    function windowResized() {
-      resizeCanvas(windowWidth, windowHeight);
-
+      letter1 = new TracingExercise();
       
     }
-  
+  //create button/clickable text for each game
+  //link exercise code
+
+  function draw() {
+    setCanvas();
+    TracingExercise.setCanvas();
+    TracingExercise.setElements();
     
+  }
+
+  function setCanvas() {
+    fill(204, 102, 0);
+    circle(windowWidth*.18, windowHeight/2, 220);
+    
+
+    textSize(300);
+    fill(0, 102, 153);
+    textAlign(CENTER, CENTER);
+    text('A', windowWidth*.50, windowHeight*.50);//tracing game letter
+ 
+    
+    image(img, windowWidth*.68, windowHeight*.40, windowWidth*.20, windowHeight*.20); //keyboard
+
+    fill(0,0,0);
+    textSize(40);
+    textAlign(CENTER);
+    text('Train Your FMS',windowWidth*.50, windowHeight*.15);
+    
+    textSize(30);
+    stroke(50);
+    textAlign(CENTER); //penny game title
+    text('Penny Pinching', windowWidth*.18, windowHeight/2);
+
+    textAlign(CENTER);//tracing title
+    text('Tracing Letters', windowWidth*.50, windowHeight/2);
+    
+    textAlign(LEFT);//typing title
+    text('Typing', windowWidth*.75, windowHeight/2);
+  }
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+
+    
+  }
+
+  
 
     
