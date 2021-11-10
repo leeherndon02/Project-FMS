970e2e80bdbe9600ae6fad07b053a6c213b4b598
Roberto De La Rosa <rjdelarosaua@gmail.com>
2021-11-09 21:01:10 -0700

    GUI Visual Changes

--- Clean Version/menu.js
+++ Clean Version/menu.js
@@ -1,21 +1,33 @@
 let title = "SkillBuilder";
 
 
+function preload(){
+  img = loadImage('assets/keyboard.png'); 
+}
 
 function setup()
 {
   createCanvas(windowWidth,windowHeight);
   background("gray");
+
   button1 = createButton('Letter Tracing');
   button1.position(windowWidth*.16, windowHeight/2);
   //button1.size(100, 100);
 
+  textSize(400);
+  fill(0, 102, 153);
+  textAlign(CENTER, CENTER);
+  text('A', windowWidth*.192, windowHeight*.51);
+
+  fill(204, 102, 0);
+  circle(windowWidth*.515, windowHeight*.51, 220);
   button2 = createButton('Penny Pinching');
   button2.position(windowWidth*.48, windowHeight/2);
   //button2.size(100,100);
 
   button3 = createButton('Typing');
   button3.position(windowWidth*.75, windowHeight/2);
+  image(img, windowWidth*.68, windowHeight*.40, windowWidth*.19, windowHeight*.20);
   //button3.size(100,100);
 
 
@@ -58,3 +70,4 @@ function draw()
   {
     window.location = "./typing.html"
   }
+
