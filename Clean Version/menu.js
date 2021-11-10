6a3f6eb045658b22ac788922ddee0a9868f15266
leeherndon02 <69217419+leeherndon02@users.noreply.github.com>
2021-11-09 20:44:01 -0700

    started part of the GUI

--- Clean Version/menu.js
+++ Clean Version/menu.js
@@ -1,23 +1,37 @@
+let title = "SkillBuilder";
+
 
 
 function setup()
 {
   createCanvas(windowWidth,windowHeight);
   background("gray");
-  button1 = createButton('Letter');
-  button1.position(windowWidth/2, windowHeight/2);
-  button1.size(100, 100);
+  button1 = createButton('Letter Tracing');
+  button1.position(windowWidth*.16, windowHeight/2);
+  //button1.size(100, 100);
 
-  button2 = createButton('Penny');
-  button2.position(windowWidth/3, windowHeight/2);
-  button2.size(100,100);
+  button2 = createButton('Penny Pinching');
+  button2.position(windowWidth*.48, windowHeight/2);
+  //button2.size(100,100);
 
   button3 = createButton('Typing');
-  button3.position(windowWidth*.667, windowHeight/2);
-  button3.size(100,100);
+  button3.position(windowWidth*.75, windowHeight/2);
+  //button3.size(100,100);
+
+
 }
 function draw()
 {
+
+    textSize(70);
+    fill('black');
+    stroke(10);
+    textAlign(CENTER);
+    fill('orange');
+    
+    text(title,windowWidth*.50, windowHeight*.15);
+
+  
   button1.mousePressed(game1);
   button2.mousePressed(game2);
   button3.mousePressed(game3);
