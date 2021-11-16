50831e80136c4a5e83079181f17cca53439d1b00
Roberto De La Rosa <rjdelarosaua@gmail.com>
2021-11-16 13:28:13 -0700

    Modfied Tracing Files

--- Clean Version/letter.js
+++ Clean Version/letter.js
@@ -1,38 +1,83 @@
-let v1;
-let v1x;
-let v1y;
-let elli1, elli2, elli3;
-let elliX, elliY, elliW, elliH;
-let rec1, rec2, rec3, rec4;
-let rectOver;
-let letter;
-let recX, recY;
-let recW = 50;
-let recH = 100;
 
-//let elliOver = false;
-//var lines = [
-//];
+
+let c, v1, v1x, v1y;
+let lines = [ ];
+let circleOver = false;
+let index = 0;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(224, 224, 224);
-    button = createButton('MENU');
-    button.position(windowWidth*.05, windowHeight*.75);
-    button.size(100, 100);
-	button.mousePressed(goBack);
+	background(220);
+	drawLetter();
+	drawCircle();
+}
+
+function draw() {
+	
+	//c = get(mouseX, mouseY); //returns rgb value where cursor hovers
+	//background(220);
+	
+	//fill(c);
+	//rect(100, 500, 100, 100);
+
+	//fill(500);
+	//textSize(20);
+	//text(c, 200, 400);
+
+
+
+	if(mouseIsPressed){
+		stroke('white');
+		//ellipse(mouseX, mouseY, 10, 10);
+		//stroke(255);
+		strokeWeight(15);
+		line(mouseX, mouseY, pmouseX, pmouseY);
+		//line(v1.x, v1.y, mouseX, mouseY);
+	}
+	
+	while (index < lines.length) {
+		stroke('white');
+		strokeWeight(15);
+		line(lines[index].x1, lines[index].y1, lines[index].x2, lines[index].y2);
+		  index += 1;
+	  }
+	  
 	
 }
 
+function drawLetter(){
+	textAlign(CENTER, CENTER);
+	strokeWeight(2);
+	textSize(700);
+	fill(50);
+	text('A', windowWidth*.50, 400);
+}
+function mousePressed(){
+	v1 = createVector(mouseX, mouseY) //need to create first line point
+	v1x = v1.x;
+	v1y = v1.y;
+  }
 
-function goBack()
-{
-  window.location = "./menu.html"
+  function drawCircle(){
+	
+  }
+
+function update() {
+	if (overCircle()) { 
+		circleOver = true;
+	} else {
+		this.circleOver = false;
+	}		  
 }
 
+function overCircle(){
+	if (mouseX >= (x-w/2) && mouseX <= (x+w/2)
+		&& mouseY >= (y-h/2) && mouseY <= (y+h/2)) {
+		return true;
+	} else {
+		return false;
+	}
+	
+}
 
-
-
-
-  
 
