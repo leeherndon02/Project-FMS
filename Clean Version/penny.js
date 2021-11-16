60c504fe0890ca7a2f422aeaac8aac098a3f0853
leeherndon02 <69217419+leeherndon02@users.noreply.github.com>
2021-11-16 13:32:02 -0700

    minor change

--- Clean Version/penny.js
+++ Clean Version/penny.js
@@ -7,7 +7,7 @@ function setup()
     button.position(windowWidth*.05, windowHeight*.75);
     button.size(100, 100);
     button.mousePressed(goBack);
-    background(220);
+    
     textSize(40);
     frameRate(10);
   }
