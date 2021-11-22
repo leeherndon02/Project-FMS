fddd11bc1db69a1f141dd6d4a88626d933ee0820
Roberto De La Rosa <rjdelarosaua@gmail.com>
2021-11-22 14:52:26 -0700

    Tracing Update

--- letter-tracing/sketch.js
+++ letter-tracing/sketch.js
@@ -12,7 +12,9 @@ let points = 0; //incremented when mouse hovers over circles
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(200);
+	//background(200);
+	fill(200);
+	rect(0,0,width, height);
 	circleX = width*.5;
 	circleY = height*.5;
 	drawLetter(); //places letter in background
@@ -22,63 +24,58 @@ function setup() {
 
 function draw() {
 
+	
+	// rectMode(CENTER);
+	// rect(windowWidth*.80, 390, 200, 200);
 	noStroke();
-	rectMode(CENTER);
-	rect(windowWidth*.80, 390, 200, 200);
-	noStroke();
+	fill(200);
 	rectMode(CENTER);
 	rect(windowWidth*.12, 390, 200, 200);
-	fill('black');
-	textSize(100);
-	text(points, windowWidth*.80, 400); //points indicator
+
+	// fill('black');
+	// textSize(100);
+	// text(points, windowWidth*.80, 400); 
 
 
 	c = get(mouseX, mouseY); //returns rgb value where cursor hovers as an array: [rgba]
-
 	textSize(20);
+	fill("black");
 	text(c, 200, 400);
-	
-
-	if(mouseIsPressed){
-		stroke('white');
-		strokeWeight(15);
-		line(pmouseX-10, pmouseY-10, pmouseX-20, pmouseY-20); //the "tracing" line that should appear
-		
-		if(c[0] == 50){
-			background('green'); //changes background color in response to change in pixel color
-			drawLetter();
-			strokeWeight(10);
-			stroke('white');
-			line(pmouseX-10, pmouseY-10, pmouseX-20, pmouseY-20);
-		}
-		else{
-			background('red');
-			drawLetter();
-			strokeWeight(10);
-			stroke('white');
-			line(pmouseX-10, pmouseY-10, pmouseX-20, pmouseY-20);
-		}
-		noStroke();
-		rectMode(CENTER);
-		rect(windowWidth*.80, 390, 200, 200);
-		
-		noStroke();
-		rectMode(CENTER);
-		rect(windowWidth*.12, 390, 200, 200);
-		fill('black');
-		textSize(100);
-		text(points, windowWidth*.80, 400);
-
-	}
 
 	noStroke();
 	drawCircle(circleX-20,circleY-250,diameter);//circle placements on letter
 	drawCircle(circleX+185,circleY+200,diameter);
 	drawCircle(circleX-80,circleY+45,diameter);
 	
-	strokeWeight(1);
-	stroke('black');
-	fill(220);
+}
+
+function mouseDragged(){
+	stroke('white');
+	strokeWeight(15);
+	line(mouseX-10, mouseY-50, mouseX-10, mouseY-50); //the "tracing" line that should appear
+	
+	if(c[0] < 51){
+		rectMode(CORNER);
+		fill('green');
+		rect(0 , 0, width, height); //changes background color in response to change in pixel color
+		drawLetter();
+
+		stroke('white');
+		strokeWeight(15);
+		line(mouseX-10, mouseY-50, mouseX-10, mouseY-50);
+
+	}
+	else{
+		rectMode(CORNER);
+		fill('red');
+		rect(0 , 0, width, height); 
+		drawLetter();
+
+		stroke('white');
+		strokeWeight(15);
+		line(mouseX-10, mouseY-50, mouseX-10, mouseY-50);
+
+		}
 }
 
 function drawLetter(){
@@ -96,11 +93,13 @@ function drawCircle(xC,yC,diam){
 
 	if (circleOver) { //changes fill color when mouse over circle
 		fill(50);
-		++points;	
+		++points;
+		
 	} 
 
 	else {
 		fill('black');
+		
 	}
 
 	circle(xC, yC, diam);
